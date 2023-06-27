32529e75597f5ec3f451601ec597db69a8972899
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2023-06-26 21:02:31 -0600

    added styles and layout

--- html_view.js
+++ html_view.js
@@ -1,8 +1,7 @@
 function updatePoints_html(points) {
-  document.getElementById("pointHeader").innerHTML = points;
+  document.getElementById("pointHeader").innerHTML = "Points: " + points;
 }
 
 function displayLogInData(userData) {
-  console.log("Here");
-  document.getElementById("streak").innerHTML = userData.streak;
+  document.getElementById("streak").innerHTML = "Streak: " + userData.streak;
 }
