9bf57afed2dc0d3ba6c22a9390a035f5a640a7ca
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2023-06-27 22:13:44 -0600

    moved buttons into collapseable

--- html_view.js
+++ html_view.js
@@ -5,3 +5,17 @@ function updatePoints_html(points) {
 function displayLogInData(userData) {
   document.getElementById("streak").innerHTML = "Streak: " + userData.streak;
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+coll[0].addEventListener("click", function () {
+  this.classList.toggle("active");
+  console.log("CLICK");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});
